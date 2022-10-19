048b5b5c25cd08a887c626ee59c4a318da2c2daf
atefe jelve <a.jelve.69@gmail.com>
2022-10-19 08:45:21 +0330

    add crispy to template

--- config/settings.py
+++ config/settings.py
@@ -38,8 +38,14 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+
+    "crispy_forms",
+
+
     "accounts",
     "pages",
+
+
 ]
 
 MIDDLEWARE = [
@@ -130,3 +136,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = 'accounts.CustomUser'
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+
+# crispy forms
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
