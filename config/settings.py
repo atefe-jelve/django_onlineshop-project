55bd8449b7353ada664b7fc2ca3b0fd03befd264
atefe jelve <a.jelve.69@gmail.com>
2022-10-24 16:27:18 +0330

    change some account templates

--- config/settings.py
+++ config/settings.py
@@ -122,6 +122,13 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = "a.jelve.69@gmail.com"
+# EMAIL_HOST_PASSWORD = "qlbbswpaisuflsqa"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
