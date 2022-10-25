aad22d06dee57b176e21c4681de63bdc6521281d
atefe jelve <a.jelve.69@gmail.com>
2022-10-25 08:27:13 +0330

    add

--- config/settings.py
+++ config/settings.py
@@ -126,8 +126,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # EMAIL_HOST = "smtp.gmail.com"
 # EMAIL_USE_TLS = True
 # EMAIL_PORT = 587
-# EMAIL_HOST_USER = "a.jelve.69@gmail.com"
-# EMAIL_HOST_PASSWORD = "qlbbswpaisuflsqa"
+# EMAIL_HOST_USER = ""
+# EMAIL_HOST_PASSWORD = ""
 
 
 # Internationalization
